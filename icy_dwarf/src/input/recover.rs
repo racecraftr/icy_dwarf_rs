802a0685c2e94afdf076cb8ef4ab065ecc584a63
Avi Gupta <savim2020@gmail.com>
2026-05-24 17:07:41 -0400

    Began recovery code progress.

---
+++ icy_dwarf/src/input/recover.rs
@@ -0,0 +1,9 @@
+use crate::input::IcyDwarfInput;
+
+impl IcyDwarfInput {
+    pub fn recover(&self, output_folder: String) -> Self {
+        let mut new_input = self.clone();
+
+        new_input
+    }
+}
