31ffb3252323429e4ff97ee7b9025b62a82cc7b3
Avi Gupta <savim2020@gmail.com>
2026-05-26 12:13:31 -0400

    Updated ThermalOut structure.
    - Moved ThermalOut from input.rs to thermal.rs.
    - Added methods to ThermalOut that return volume calculations and total
    mass.

--- icy_dwarf/src/thermal.rs
+++ icy_dwarf/src/thermal.rs
@@ -1,3 +1,10 @@
+use std::{cell::OnceCell, fs, sync::Once};
+
+use crate::{
+    consts::{GRAM, KM, KM2CM, RHO_ADHS, RHO_H2OL, RHO_NH3L},
+    input::IcyDwarfInput,
+    planet_system::{RHO_ADHS_TH, RHO_H2OL_TH, RHO_NH3L_TH, WorldState},
+};
 use faer::{
     Mat,
     linalg::solvers::{DenseSolveCore, Svd},
@@ -5,13 +12,10 @@ use faer::{
 };
 use num::complex::Complex64;
 
-use crate::{
-    consts::{EROCK_A, EROCK_C, EROCK_D, EROCK_F},
-    input::IcyDwarfInput,
-};
+pub const N_THERM: usize = 14;
 
 impl IcyDwarfInput {
-    pub fn thermal(&self) {
+    pub fn thermal(&self, world_state: &mut WorldState) {
         let tilt_scale: f64 = 1.0;
     }
 
@@ -23,18 +27,8 @@ impl IcyDwarfInput {
     }
 
     fn hydrate(&self) {}
-}
 
-fn heat_rock(T: f64) -> f64 {
-    if T > 1000.0 {
-        EROCK_A * 275.0 * 275.0
-            + (1000.0 - 275.0) * (EROCK_C + EROCK_D * 1000.0)
-            + (EROCK_F) * (T - 1000.0)
-    } else if T > 275.0 {
-        EROCK_A * 275.0 * 275.0 + (T - 275.0) * (EROCK_C + EROCK_C * T)
-    } else {
-        EROCK_A * T * T
-    }
+    fn prop_mtx(&self) {}
 }
 
 fn svd(mat: &Vec<Vec<f64>>) -> Option<Svd<f64>> {
@@ -87,4 +81,87 @@ mod test {
         println!("U = {:?}", svd.U()); // U represents U
         println!("V = {:?}", svd.V().conjugate()); // V represents (V^*)^*
     }
+}
+
+#[derive(Clone, Debug, Default)]
+pub struct ThermalOut {
+    pub radius_km: f64,
+    pub temp_kelvin: f64,
+    pub mass_rock: f64,
+    pub mass_ice: f64,
+    pub mass_ammonia_solid: f64,
+    pub mass_water: f64,
+    pub mass_ammonia_liquid: f64,
+    pub nusselt_num: f64,
+    pub ice_frac_amorphous: f64,
+    pub thermal_cond: f64,
+    pub deg_of_hydr: f64,
+    pub porosity: f64,
+    pub crack: bool,
+    pub tidal_heating_rate: f64,
+}
+
+type ThermVol = (f64, (f64, f64, f64, f64, f64));
+
+impl ThermalOut {
+    fn from_line(ln: &str) -> Option<Self> {
+        let parts = ln.trim().split_whitespace().collect::<Vec<_>>();
+        let radius_km = parts[0].parse::<f64>().ok()?;
+        let radius_km = radius_km * KM2CM;
+        Some(Self {
+            radius_km,
+            temp_kelvin: parts[1].parse().ok()?,
+            mass_rock: parts[2].parse().ok()?,
+            mass_ice: parts[3].parse().ok()?,
+            mass_ammonia_solid: parts[4].parse().ok()?,
+            mass_water: parts[5].parse().ok()?,
+            mass_ammonia_liquid: parts[6].parse().ok()?,
+            nusselt_num: parts[7].parse().ok()?,
+            ice_frac_amorphous: parts[8].parse().ok()?,
+            thermal_cond: parts[9].parse().ok()?,
+            deg_of_hydr: parts[10].parse().ok()?,
+            porosity: parts[11].parse().ok()?,
+            crack: parts[12].parse::<u8>().map(|n| n == 1).ok()?,
+            tidal_heating_rate: parts[13].parse().ok()?,
+        })
+    }
+
+    pub fn mass_total(&self) -> f64 {
+        self.mass_rock + self.mass_ice + self.mass_ammonia_solid + self.mass_ammonia_liquid
+    }
+
+    pub fn vol(&self, input: &IcyDwarfInput) -> ThermVol {
+        let vol_rock = self.mass_rock
+            / (self.deg_of_hydr * input.world_spec.rho_hydr_th()
+                + (1.0 - self.deg_of_hydr) * input.world_spec.rho_rock_th());
+        let vol_ice = self.mass_ice / RHO_H2OL_TH;
+        let vol_adhs = self.mass_ammonia_solid / RHO_ADHS_TH;
+        let vol_water = self.mass_water / RHO_H2OL_TH;
+        let vol_nh3l = self.mass_ammonia_liquid / RHO_NH3L_TH;
+        (
+            vol_rock + vol_adhs + vol_water + vol_nh3l,
+            (vol_rock, vol_ice, vol_adhs, vol_water, vol_nh3l),
+        )
+    }
+}
+
+fn read_thermal_out(path: &str, output_time_step: usize) -> Option<Vec<Vec<ThermalOut>>> {
+    let Ok(lines) =
+        fs::read_to_string(path).map(|s| s.lines().map(str::to_owned).collect::<Vec<_>>())
+    else {
+        return None;
+    };
+    Some(
+        lines
+            .chunks(output_time_step)
+            // we don't need to define NT
+            // as a pamaeter, as it is sized dynamically.
+            .map(|chunk| {
+                chunk
+                    .iter()
+                    .filter_map(|ln| ThermalOut::from_line(ln))
+                    .collect()
+            })
+            .collect(),
+    )
 }
