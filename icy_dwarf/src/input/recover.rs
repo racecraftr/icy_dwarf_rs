88573e440dc96194c653a712e315ccb899363f80
Avi Gupta <savim2020@gmail.com>
2026-05-26 12:23:26 -0400

    Begun recovery implementation.

--- icy_dwarf/src/input/recover.rs
+++ icy_dwarf/src/input/recover.rs
@@ -1,9 +1,14 @@
-use crate::input::IcyDwarfInput;
+use crate::{input::IcyDwarfInput, thermal::ThermalOut};
+
+const N_THERM: usize = 14;
+const N_ORBIT: usize = 12;
+const N_CRK_STRS: usize = 12;
+const N_HEAT: usize = 10;
+const N_REBOUND: usize = 7;
 
 impl IcyDwarfInput {
-    pub fn recover(&self, output_folder: String) -> Self {
-        let mut new_input = self.clone();
-
-        new_input
+    pub fn recover(&self, output_folder: String) {
+        let nr = self.grid.n_zones;
+        let mut thermal_out: ThermalOut = ThermalOut::default();
     }
 }
