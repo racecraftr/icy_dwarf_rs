6c3f8b03fe338fc99d9756720d02ac780434c32e
Avi Gupta <zeroterabytes+github@pm.me>
2026-07-31 17:51:49 -0400

    Add documentation.

--- icy_dwarf/build.rs
+++ icy_dwarf/build.rs
@@ -1,5 +1,9 @@
+//! This build script configures Cargo to link the `iphreeqc` C++ library into the Rust binary executable.
+
+/// Configure cargo native library search paths and dynamic linker flags.
 fn main() {
     println!("cargo:rustc-link-search=native=/usr/local/lib");
     println!("cargo:rustc-link-lib=iphreeqc");
     println!("cargo:rustc-link-arg=-Wl,-rpath,/usr/local/lib");
 }
+
